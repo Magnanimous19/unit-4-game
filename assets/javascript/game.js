7539407fe24c8bfb6646fcd9f13b0f152e35c61f
Zaku <reidburnsy19@hotmail.com>
2020-02-26 11:53:58 -0500

    Basic layout for each file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,29 @@
-java
+
+
+//Logic for random number for each crystal (1-12) and starting value (19-120)
+//--============================================================-
+
+//Random Starting Value 
+var targetMin = 19
+var targetMax = 120
+var targetNumber = Math.floor(Math.random() * (+targetMax - +targetMin)) + +targetMin;
+document.write("Random number test" + targetNumber);
+
+//Random Value for each crystal
+
+
+
+//Id imgs  Crystals
+//==============================================================-->
+
+
+
+//Logic for clicking each crystal and buttons-->
+//=============================================================-->
+
+
+
+//Adding and saving random crystal number-->
+//=========================================================-->
+
+
